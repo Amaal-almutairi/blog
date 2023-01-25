dcc7b8970298dec3fb1fae5cb8f4c686aec04f06
amaal <amaal772@gmail.com>
2023-01-25 20:39:48 +0300

    Initial Commit

---
+++ blog/blogApp.swift
@@ -0,0 +1,17 @@
+//
+//  blogApp.swift
+//  blog
+//
+//  Created by Amaal Almutairi on 03/07/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct blogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
